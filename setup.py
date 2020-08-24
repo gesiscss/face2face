d7c9889e919fc668690c00d34337e0b2335ff362
Andy <Andreas.Kruff@smail.th-koeln.de>
2020-08-24 10:44:23 +0200

    Updated setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, Extension
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+DEPENDENCIES = ['numpy', 'pandas', 'matplotlib', 'networkx', 'scipy',
+                'seaborn', 'powerlaw']
+packages = ["face2face",
+            "face2face.imports",
+            "face2face.statistics",
+            "face2face.visualization",
+	    "face2face.compatibility_check"
+            ]
+setup(name="face2face",
+      version="0.1.1.1.3 Alpha",
+      packages=packages,
+      description="Library with basic social science functions",
+      url="https://gesiscss.github.io/face2face/",
+      #python_requires=">=3.6.1",
+      long_description=long_description,
+      install_requires=DEPENDENCIES,
+      package_data = {"face2face": ["data/*/*.dat"]}
+      )
